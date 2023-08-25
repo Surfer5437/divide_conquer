da2cade9862006be54a427be125b81c3b7128196
Surfer5437 <livingstonebrandon5437@gmail.com>
2023-08-25 18:03:59 -0400

    sync completed and updated for next project

--- divide_conquer.js
+++ divide_conquer.js
@@ -67,3 +67,136 @@ function findRotationCount(arr){
 findRotationCount([15, 18, 2, 3, 6, 12]) // 2
 findRotationCount([7, 9, 11, 12, 5]) // 4
 findRotationCount([7, 9, 11, 12, 15]) // 0
+
+// ********************     stack    ***************************
+
+/** Node: node for a stack. */
+
+class Node {
+    constructor(val) {
+      this.val = val;
+      this.next = null;
+    }
+  }
+  
+  /** Stack: chained-together nodes where you can
+   *  remove from the top or add to the top. */
+  class Stack {
+    constructor() {
+      this.first = null;
+      this.last = null;
+      this.size = 0;
+    }
+  
+    /** push(val): add new value to end of the stack. Returns undefined. */
+  
+    push(val) {
+      let node = new Node(val);
+  
+      if (!this.first) {
+        this.first = node;
+        this.last = node;
+      } else {
+        let tmp = this.first;
+        this.first = node;
+        this.first.next = tmp;
+      }
+  
+      this.size++;
+    }
+  
+    /** pop(): remove the node from the top of the stack
+     * and return its value. Should throw an error if the stack is empty. */
+  
+    pop() {
+      if (!this.first) throw new Error("Can't pop from an empty stack.");
+  
+      let temp = this.first;
+  
+      if (this.first == this.last) {
+        this.last = null;
+      }
+  
+      this.first = this.first.next;
+      this.size--;
+      return temp.val;
+    }
+  
+    /** peek(): return the value of the last node in the stack. */
+  
+    peek() {
+      return this.first.val;
+    }
+  
+    /** isEmpty(): return true if the stack is empty, otherwise false */
+  
+    isEmpty() {
+      return this.size === 0;
+    }
+  }
+  
+
+  // *************************    Queue    ************************
+
+  /** Node: node for a queue. */
+
+class Node {
+    constructor(val) {
+      this.val = val;
+      this.next = null;
+    }
+  }
+  
+  
+  /** Queue: chained-together nodes where you can
+   *  remove from the front or add to the back. */
+  class Queue {
+    constructor() {
+      this.first = null;
+      this.last = null;
+      this.size = 0;
+    }
+  
+    /** enqueue(val): add new value to end of the queue. Returns undefined. */
+  
+    enqueue(val) {
+      let node = new Node(val);
+  
+      if (!this.first) {
+        this.first = node;
+        this.last = node;
+      } else {
+        this.last.next = node;
+        this.last = node;
+      }
+  
+      this.size++;
+    }
+  
+    /** dequeue(): remove the node from the start of the queue
+     * and return its value. Should throw an error if the queue is empty. */
+  
+    dequeue() {
+      if (!this.first) throw new Error("Can't dequeue from an empty queue.");
+  
+      let temp = this.first;
+      if (this.first == this.last) {
+        this.last = null;
+      }
+      this.first = this.first.next;
+      this.size--;
+      return temp.val;
+    }
+  
+    /** peek(): return the value of the first node in the queue. */
+  
+    peek() {
+      return this.first.val;
+    }
+  
+    /** isEmpty(): return true if the queue is empty, otherwise false */
+  
+    isEmpty() {
+      return this.size === 0;
+    }
+  }
