5abf1f30276b7e93a62fb4b2c2e273ec7f91e2d8
Surfer5437 <livingstonebrandon5437@gmail.com>
2023-08-27 16:44:02 -0400

    sync

--- divide_conquer.js
+++ divide_conquer.js
@@ -140,7 +140,7 @@ class Node {
 
   /** Node: node for a queue. */
 
-class Node {
+class Node2 {
     constructor(val) {
       this.val = val;
       this.next = null;
@@ -160,7 +160,7 @@ class Node {
     /** enqueue(val): add new value to end of the queue. Returns undefined. */
   
     enqueue(val) {
-      let node = new Node(val);
+      let node = new Node2(val);
   
       if (!this.first) {
         this.first = node;
@@ -200,3 +200,113 @@ class Node {
       return this.size === 0;
     }
   }
+
+
+
+  // function realSize(arrays, x=0, pos=0) {
+  //   //   [1,2,[3,4]]            1      1
+    
+  //   if (Array.isArray(arrays[pos])){
+  //     x+=realSize(arrays[pos],x)
+  //   } else if (Number.isInteger(arrays[pos])){
+  //     realSize(arrays,x+=1,pos+=1)
+  //   } else if (arrays[pos]=== undefined){
+     
+  //     console.log(x);
+  //      return x;
+  //   }
+  //   }
+  function realSize(arrays, x=0, pos=0) {
+    
+    if (pos == arrays.length) return x;
+  
+        for (let i = pos; i < arrays.length; i++){
+          const element = arrays[i];
+          if (Array.isArray(element)) {
+            x += realSize(arrays[pos], x)
+          } else {
+            x+=1;
+          }
+        }
+   if (pos == arrays.length) return x;
+  
+    }
+    console.log(realSize([1,2,[3,4]]));
+
+
+    /** product: calculate the product of an array of numbers. */
+
+function product(nums, idx = 0) {
+  if (idx === nums.length) return 1;
+  return nums[idx] * product(nums, idx + 1);
+}
+
+/** longest: return the length of the longest word in an array of words. */
+
+function longest(words, idx = 0, longestSoFar = 0) {
+  if (idx === words.length) return longestSoFar;
+  longestSoFar = Math.max(words[idx].length, longestSoFar);
+  return longest(words, idx + 1, longestSoFar);
+}
+
+/** everyOther: return a string with every other letter. */
+
+function everyOther(str, idx = 0, newStr = "") {
+  if (idx >= str.length) return newStr;
+  newStr += str[idx];
+  return everyOther(str, idx + 2, newStr);
+}
+
+/** isPalindrome: checks whether a string is a palindrome or not. */
+
+function isPalindrome(str, idx = 0) {
+  let leftIdx = idx;
+  let rightIdx = str.length - idx - 1;
+  if (leftIdx >= rightIdx) return true;
+  if (str[leftIdx] !== str[rightIdx]) return false;
+  return isPalindrome(str, idx + 1);
+}
+
+/** findIndex: return the index of val in arr (or -1 if val is not present). */
+
+function findIndex(arr, val, idx = 0) {
+  if (idx === arr.length) return -1;
+  if (arr[idx] === val) return idx;
+  return findIndex(arr, val, idx + 1);
+}
+
+/** revString: return a copy of a string, but in reverse. */
+
+function revString(str, idx = 0, newStr = "") {
+  if (newStr.length === str.length) return newStr;
+  newStr += str[str.length - 1 - idx];
+  return revString(str, idx + 1, newStr);
+}
+
+/** gatherStrings: given an object, return an array of all of the string values. */
+
+function gatherStrings(obj) {
+  let stringArr = [];
+  for (let key in obj) {
+    if (typeof obj[key] === "string") stringArr.push(obj[key]);
+    if (typeof obj[key] === "object") stringArr.push(...gatherStrings(obj[key]));
+  }
+  return stringArr;
+}
+
+/** binarySearch: given a sorted array of numbers, and a value,
+ * return the index of that value (or -1 if val is not present). */
+
+function binarySearch(arr, val, left = 0, right = arr.length) {
+  if (left > right) {
+    return -1;
+  }
+  let middle = Math.floor((right + left) / 2);
+  if (arr[middle] === val) {
+    return middle;
+  }
+  if (arr[middle] > val) {
+    return binarySearch(arr, val, left, middle - 1);
+  }
+  return binarySearch(arr, val, middle + 1, right);
+}
